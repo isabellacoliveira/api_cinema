c9f5b58e13289005239a0ac76d65044f9ea7712f
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-02-17 14:01:02 -0300

    gerando migrations e criando o banco de dados

--- FilmesAPI/Data/FilmesContext.cs
+++ FilmesAPI/Data/FilmesContext.cs
@@ -5,18 +5,11 @@ namespace FilmesAPI.Data;
 
 public class FilmeContext : DbContext
 {
-    // ele vai receber como parâmetro uma configuração de acesso ao banco 
-    // vamos fazer a passagem dessa opçoes para o método construtor que estamos 
-    // vamos fazer isso através do base 
     public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)
     {
         
     }
 
-    // precisamos criar a propriedades de acesso 
-    // db set : conjuntos de dados do nosso banco 
-    // vai conter os filmes 
     public DbSet<Filme> Filmes { get; set; }
 
-    // precisamos autenticar a aplicação ao banco 
 }
--- FilmesAPI/models/Filme.cs
+++ FilmesAPI/models/Filme.cs
@@ -4,6 +4,10 @@ namespace FilmesAPI.Models;
 
 public class Filme 
 {
+    // queremos dizer que o id será uma chave para uma tabela no banco de dados 
+    // precisamos dizer para o dotnet fazer um mapeamento para jogar isso no banco de dados
+    [Key]
+    [Required]
     public int Id { get; set; }
     [Required(ErrorMessage = "O título do filme é obrigatório!")]
     public string Titulo { get; set; }
